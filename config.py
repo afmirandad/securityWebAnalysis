f2697bee3d2fc33656dbde28816a5a305fb956ec
afmirandadiaz <amirandadiaz@godaddy.com>
2024-09-24 10:47:01 -0500

    Second Commit

--- config.py
+++
@@ -1,5 +0,0 @@
-config = {
-    "uri":"mongodb+srv://andrewdiazmiranda:lR407oYwyPJ33UNM@cluster0.7dlwb0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    "database":"cienciadedatos702",
-    "collection":"cybersecurity"
-}
--- webLogAnalysis.py
+++ webLogAnalysis.py
@@ -8,15 +8,20 @@ class classifiedData:
     def __init__(self):
         self.directory = 'logsDownloaded'
         self.url = "http://www.secrepo.com/self.logs/access.log.2017-01-01.gz" #Url de archivo de log comprimido
+        self.local_file_gz = os.path.join(self.directory,'access.log.2017-01-01.gz')
         self.validateFolder()
 
     def downloadFile(self):
-        local_file_gz = os.path.join(self.directory,'access.log.2017-01-01.gz')
         responseData = requests.get(self.url, stream=True, verify=False)
-        with open(local_file_gz,'wb') as file:
+        with open(self.local_file_gz,'wb') as file:
             for chunk in responseData.iter_content(chunk_size=1024):
                 if chunk:
                     file.write(chunk)
+
+    def decompressFile(self):
+        with gzip.open(self.local_file_gz, 'rt') as file:
+            file_content = file.read()
+            print(file_content)
 
     def validateFolder(self):
         if not os.path.exists(self.directory):
@@ -24,3 +29,4 @@ class classifiedData:
 
 prueba = classifiedData()
 prueba.downloadFile()
+prueba.decompressFile()
