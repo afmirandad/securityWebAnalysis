63370860ff5e77fcff67cf1d2b8dabbae35e54d6
afmirandadiaz <amirandadiaz@godaddy.com>
2024-10-02 09:48:37 -0500

    Another commit

--- clean&PrepareData.py
+++ clean&PrepareData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os, re
+from datetime import datetime
 
 class CleanUp:
 
@@ -10,13 +11,39 @@ class CleanUp:
 )
 
     def cleanUpLog(self):
-        for file in os.listdir():
+        pathRoute = os.getcwd()
+        for file in os.listdir(pathRoute):
             if file.endswith('txt'):
-                filefullpath = os.path.join(file)
-                with open(file, 'r', encoding='utf-8') as rawFile:
-                    pass
+                fullPath = os.path.join(pathRoute,file)
+                with open(fullPath, 'r', encoding='utf-8') as rawFile:
+                    for line in rawFile:
+                        self.listJson.append(self.regexClean(line))
+                    print(self.listJson)
 
+    def regexClean(self,file):
+        logs_json = []
+        for match in re.finditer(self.log_pattern, file):
+            ip = match.group('ip')
+            date_str = match.group('date')
+            method = match.group('method')
+            resource = match.group('resource')
+            error_code = match.group('error_code')
 
+            # Convertir la fecha al formato deseado
+            date_time = datetime.strptime(date_str.split()[0], "%d/%b/%Y:%H:%M:%S")
+
+            # Crear el diccionario para cada entrada
+            json_data = {
+                'date': date_time.strftime('%Y-%m-%d'),
+                'ip': ip,
+                'method': method,
+                'resource': resource,
+                'errorCode': error_code
+            }
+
+            # Añadir la entrada JSON a la lista
+            logs_json.append(json_data)
+        return logs_json
 
 
 class PrepareData:
